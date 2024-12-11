d86898d5953d5aa89d806510098086d46b418eb8
Junaid Syed <sjunaid626@gmail.com>
2024-12-11 13:13:09 +0530

    downgrade react version

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { formatAmount } from '../lib/utils'
+import AnimatedCounter from './AnimatedCounter'
 
 const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }): TotalBalanceBoxProps => {
     return (
-        <section className='total-balance'>
-            <div className='total-balance-chart'>
-
+        <section className="total-balance">
+            <div className="total-balance-chart">
+                {/* <DoughnutChart accounts={accounts} /> */}
             </div>
-            <div className='flex flex-col gap-6'>
-                <h2 className='header-2'>
+
+            <div className="flex flex-col gap-6">
+                <h2 className="header-2">
                     Bank Accounts: {totalBanks}
                 </h2>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <p className='total-balance-label'>
-                    Total Current Balance: ${totalCurrentBalance}
-                </p>
-                <p className='total-balance-amount flex-center gap-2'>
-                    {formatAmount(totalCurrentBalance)}
-                </p>
+                <div className="flex flex-col gap-2">
+                    <p className="total-balance-label">
+                        Total Current Balance
+                    </p>
+
+                    <div className="total-balance-amount flex-center gap-2">
+                        <AnimatedCounter amount={totalCurrentBalance} />
+                    </div>
+                </div>
             </div>
         </section>
     )

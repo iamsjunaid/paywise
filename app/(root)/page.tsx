f612d9eaf54635c6c51df8520064e5f843e6e049
Junaid Syed <sjunaid626@gmail.com>
2024-12-13 20:02:44 +0530

    display card info on the sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/HeaderBox"
+import RightSideBar from "@/components/RightSideBar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
-  const loggedIn = { firstName: "Adrian" }
+  const loggedIn = { firstName: "Adrian", lastName: "Jason", email: 'contact@jsmastery.pro' }
 
   return (
     <section className='home'>
@@ -14,13 +15,22 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
+
           <TotalBalanceBox
             accounts={[]}
             totalBalance={1}
             totalCurrentBalance={1250.35}
           />
+
+
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSideBar
+        user= {loggedIn}
+        transactions = {[]}
+        banks = {[{currentBalance: 123.50},{ currentBalance: 500.00 }]}
+      />
     </section>
   )
 }
